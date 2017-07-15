42d54e876614b3e502e06c6d8f5f1c78a75fb297
AlexKnauth <alexander@knauth.org>
2017-07-15 17:28:12 -0400

    add missing dependencies

--- info.rkt
+++ info.rkt
@@ -4,5 +4,15 @@
 
 (define deps
   '("base"
+    "agile"
+    "collections-lib"
+    "htdp-lib"
+    "math-lib"
+    "txexpr"
+    ))
+
+(define build-deps
+  '("at-exp-lib"
+    "rackunit-lib"
     ))
 
