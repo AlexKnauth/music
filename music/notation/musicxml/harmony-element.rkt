6eb423361d7a7a87f105193bcbf37bd4ae419aac
AlexKnauth <alexander@knauth.org>
2018-05-20 20:09:00 -0400

    use musicxml package more (harmony, frame, etc.)

--- music/notation/musicxml/harmony-element.rkt
+++ music/notation/musicxml/harmony-element.rkt
@@ -1,7 +1,7 @@
 #lang agile
 
 (require racket/bool
-         (submod txexpr safe)
+         musicxml/harmony
          (prefix-in data/
            (combine-in
             music/data/note/note
@@ -21,12 +21,12 @@
       layout)
      (cond
        [(false? layout)
-        (txexpr 'harmony '([print-frame "no"])
+        (harmony '([print-frame "no"])
           (list
            (harmony-root->musicxml r)
            (harmony-kind->musicxml k)))]
        [else
-        (txexpr 'harmony '([print-frame "no"])
+        (harmony '([print-frame "no"])
           (list
            (harmony-root->musicxml r)
            (harmony-kind->musicxml k)
@@ -37,20 +37,20 @@
   (define n (data/note-name-string r))
   (define a (data/note-alteration r))
   (cond [(zero? a)
-         (txexpr 'root '()
+         (root '()
            (list
-            (txexpr 'root-step '() (list n))))]
+            (root-step '() (list n))))]
         [else
-         (txexpr 'root '()
+         (root '()
            (list
-            (txexpr 'root-step '() (list n))
-            (txexpr 'root-alter '() (list (number->string a)))))]))
+            (root-step '() (list n))
+            (root-alter '() (list (number->string a)))))]))
 
 ;; harmony-kind->musicxml : ChordSymbolKind -> MXexpr
 (define (harmony-kind->musicxml k)
   (match k
     [(data/chord-symbol-kind kind-str _)
-     (txexpr 'kind '() (list kind-str))]))
+     (kind '() (list kind-str))]))
 
 ;; harmony-frame->musicxml : ChordLayout -> MXexpr
 (define (harmony-frame->musicxml chord-layout)
@@ -58,18 +58,18 @@
   (define k
     (add1 (apply max 0
             (map data/ivl-midi∆ (filter values chord-layout)))))
-  (txexpr 'frame '()
+  (frame '()
     (list*
-     (txexpr 'frame-strings '() (list (number->string n)))
-     (txexpr 'frame-frets '() (list (number->string k)))
+     (frame-strings '() (list (number->string n)))
+     (frame-frets '() (list (number->string k)))
      (for/list ([s (in-list chord-layout)]
                 [i (in-range n 0 -1)]
                 #:when s)
        (define f (data/ivl-midi∆ s))
-       (txexpr 'frame-note '()
+       (frame-note '()
          (list
-          (txexpr 'string '() (list (number->string i)))
-          (txexpr 'fret '() (list (number->string f)))))))))
+          (string '() (list (number->string i)))
+          (fret '() (list (number->string f)))))))))
 
 ;; ------------------------------------------------------------------------
 
